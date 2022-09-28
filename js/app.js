3f0f0096ae5fc950c879c34980cce821b1a942a7
Sally <proto1998@gmail.com>
2022-09-28 17:27:03 +0200

    First Commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+const ratio = .5
+
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
+
+const handleIntersect = function (entries, observer) {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+};
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+document.querySelectorAll('.reveal-L, .reveal-R').forEach(function (r) {
+    observer.observe(r)
+})
+
+
